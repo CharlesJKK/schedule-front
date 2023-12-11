99ed7aad3a5552055456c1641f9e0349b38ebf39
Charles Junior <charles.sales@townsq.com.br>
2023-12-11 07:07:10 -0300

    back

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-// Função para criar um card de agendamento
-function criarAgendamento(modalidade, hora, data) {
-    if (!modalidade || !hora || !data) {
-        alert('Por favor, preencha todos os campos antes de adicionar um agendamento.');
-        return; // Não cria um agendamento vazio
+    // Função para criar um card de agendamento
+    function criarAgendamento(modalidade, hora, data) {
+        if (!modalidade || !hora || !data) {
+            alert('Por favor, preencha todos os campos antes de adicionar um agendamento.');
+            return; // Não cria um agendamento vazio
+        }
+
+        const agendamentoCard = document.createElement('div');
+        agendamentoCard.className = 'agendamento-card';
+
+        const modalidadeElement = document.createElement('h2');
+        modalidadeElement.textContent = `Modalidade: ${modalidade}`;
+
+        const horaElement = document.createElement('p');
+        horaElement.textContent = `Hora: ${hora}`;
+
+        const dataElement = document.createElement('p');
+        dataElement.textContent = `Data: ${data}`;
+
+        const botaoAgendar = document.createElement('a');
+        botaoAgendar.textContent = 'Consultar';
+        botaoAgendar.href = 'javascript:void(0)'; // Evita a navegação para outra página
+        botaoAgendar.className = 'botao-agendar'; // Aplicando a classe para o botão
+
+        // Adicionando um evento de clique ao botão "Consultar"
+        botaoAgendar.addEventListener('click', function () {
+            // Exibir um popup de confirmação
+            const confirmacao = `Agendamento confirmado para ${hora} em ${data}.`;
+            alert(confirmacao);
+        });
+
+        agendamentoCard.appendChild(modalidadeElement);
+        agendamentoCard.appendChild(horaElement);
+        agendamentoCard.appendChild(dataElement);
+        agendamentoCard.appendChild(botaoAgendar);
+
+        document.getElementById('cards-container').appendChild(agendamentoCard);
     }
-
-    const agendamentoCard = document.createElement('div');
-    agendamentoCard.className = 'agendamento-card';
-
-    const modalidadeElement = document.createElement('h2');
-    modalidadeElement.textContent = `Modalidade: ${modalidade}`;
-
-    const horaElement = document.createElement('p');
-    horaElement.textContent = `Hora: ${hora}`;
-
-    const dataElement = document.createElement('p');
-    dataElement.textContent = `Data: ${data}`;
-
-    const botaoAgendar = document.createElement('a');
-    botaoAgendar.textContent = 'Consultar';
-    botaoAgendar.href = 'pagina_de_agendamentos.html'; // Insira o URL da página de agendamentos aqui
-    botaoAgendar.className = 'botao-agendar'; // Aplicando a classe para o botão
-
-    agendamentoCard.appendChild(modalidadeElement);
-    agendamentoCard.appendChild(horaElement);
-    agendamentoCard.appendChild(dataElement);
-    agendamentoCard.appendChild(botaoAgendar);
-
-    document.getElementById('cards-container').appendChild(agendamentoCard);
-}
 
 // Função para abrir um formulário ou modal para inserir os detalhes do agendamento
 function adicionarAgendamento() {
